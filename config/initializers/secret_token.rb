4b7ae2e259fb7ebaaf9cd336d780d3a0a3a43fd7
spaceweb <spaceweb@outlook.com>
2013-08-30 07:46:06 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '02f528c7d1d3ef823bd036fa0a0cb413c1f9b728edc72912c766c750dcade931a94551eef9c3b39a44a23523ea98be32b568c6d5daf5af987c44b99407170d1a'
